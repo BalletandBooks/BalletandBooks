50c7716159442e1c41edba34bac22d3867386b24
Brian Zhang <zhangbri@umich.edu>
2025-03-24 20:44:35 -0400

    Old calendar added

--- new/src/app/home/page.tsx
+++ new/src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Link from 'next/link';
 const NavBar: React.FC = () => {
     return (
         <nav style={styles.nav}>
@@ -8,6 +8,7 @@ const NavBar: React.FC = () => {
                 <li style={styles.li}><a href="#about" style={styles.a}>About</a></li>
                 <li style={styles.li}><a href="#services" style={styles.a}>Services</a></li>
                 <li style={styles.li}><a href="#contact" style={styles.a}>Contact</a></li>
+                <li style={styles.li}><Link href="/calendar"><a style={styles.a}>Calendar</a></Link></li>
             </ul>
         </nav>
     );
