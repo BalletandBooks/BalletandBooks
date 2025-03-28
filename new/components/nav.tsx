b95af0a0f629c7167ccf3c77fca464b887181f8d
YuFeng Lin <a382259104@gmail.com>
2025-03-28 19:30:13 -0400

    css update to nav bar, need user authentification for different navbar functions

--- new/components/nav.tsx
+++ new/components/nav.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './nav-css.css';
+import balletAndBooks from '../assets/logo.png.webp';
 
 const Nav: React.FC = () => {
     return (
         <nav className="navbar">
-            <div className="logo">Logo</div>
-            <div className="nav-links">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact</a>
+            <div className="logo-container">
+                <img src={balletAndBooks.src} alt="Logo" className="logo-image" />
+            </div>
+            <ul className="nav-links">
+                <li><a href="/Curriculum">Curriculum</a></li>
+                <li><a href="/Students">Students</a></li>
+                <li><a href="/Training">Training</a></li>
+                <li><a href="/Documents">Documents</a></li>
+                <li><a href="/Calendar">Calendar</a></li>
+            </ul>
+            <div className="profile-container">
+                <span className="profile-name">YuFeng Lin :)</span>
+
+                <div className="profile-picture">
+                    {/* Profile picture placeholder */}
+                </div>
             </div>
         </nav>
     );
--- new/src/app/home/page.tsx
+++ new/src/app/home/page.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import Nav from "../../../components/nav";
 
+const HomePage = () => {
+    return (
+        <div style={{ backgroundColor: 'white', minHeight: '100vh' }}>
+            <Nav />
+        </div>
+    );
+};
+
 export default Nav;
