cb571c80445cf91816c539f48439481d70b16796
shahdalg <shahdal@umich.edu>
2025-04-07 21:03:06 -0400

    students page blank

---
+++ new/src/app/students/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import NavBar from '../../../components/nav';
+
+const StudentsPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Students Page</h1>
+      <p>This is a placeholder for the students page.</p>
+    </div>
+  );
+};
+
+export default StudentsPage;
