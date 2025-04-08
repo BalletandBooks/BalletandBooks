0384939dcf303030e11d433076a1cd265e252186
shahdalg <shahdal@umich.edu>
2025-04-07 21:06:02 -0400

    training page (no actual content)

---
+++ new/src/app/training/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import NavBar from '../../../components/nav';
+
+const TrainingPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Training Page</h1>
+      <p>This is a placeholder for the training page.</p>
+    </div>
+  );
+};
+
+export default TrainingPage;
