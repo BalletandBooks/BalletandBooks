3377791a49cc6a7f660ffac852f797704b86d319
shahdalg <shahdal@umich.edu>
2025-04-07 20:03:46 -0400

    Added navbar to calendar

--- new/src/app/calendar/page.tsx
+++ new/src/app/calendar/page.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import NavBar from '../../../components/nav';
 
 const CalendarScreen = () => {
   return (
     <div>
-      <h1>Calendar</h1>
-      <iframe
+      <NavBar />
+      <div style={{ padding: '20px' }}>
+        <h1>Calendar</h1>
+        <iframe
         src="https://calendar.google.com/calendar/embed?src=your_calendar_id&ctz=Your_Time_Zone"
         style={{ border: 0, width: '100%', height: '600px' }}
         frameBorder="0"
         scrolling="no"
       ></iframe>
+      </div>
     </div>
   );
 };
