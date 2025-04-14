7f00c79452b3dc59420d7c19428d3b16075a1dbc
shahdalg <shahdal@umich.edu>
2025-04-14 19:34:25 -0400

    documents loads now

---
+++ new/src/app/documents/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import NavBar from '../../../components/nav';
+
+const DocumentsPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Documents Page</h1>
+      <p>This is a placeholder for the documents page.</p>
+    </div>
+  );
+};
+
+export default DocumentsPage;
