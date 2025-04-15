a1dfecd12b5e30952fef729d6cfcb81bee6226f5
shahdalg <shahdal@umich.edu>
2025-04-14 20:10:03 -0400

    profile page loads

---
+++ new/src/app/profile/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import NavBar from '../../../components/nav';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Profile Page</h1>
+      <p>This is a placeholder for the profile page.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
